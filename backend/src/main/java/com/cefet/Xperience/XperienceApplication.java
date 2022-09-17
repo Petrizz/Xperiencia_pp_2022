b11666bdab265b9b082aa0e7a49405e70964a012
Petrizz <pedrogonzagamendesoficial@gmail.com>
2022-09-17 12:18:49 -0300

    backend add -v17

---
+++ backend/src/main/java/com/cefet/Xperience/XperienceApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.Xperience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XperienceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cefet/Xperience/XperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.Xperience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
